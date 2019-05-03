ea89d21a36b1ecc32eb9bea303001c8c40945de9
Peter Talley <peterctalley@gmail.com>
2019-05-03 10:17:26 -0400

    Update and simplify OpenSeesAnalysis

--- opswrapper/analysis.py
+++ opswrapper/analysis.py
@@ -1,21 +1,15 @@
 """Helpers for running OpenSees."""
 
 import dataclasses
-import logging
-import os
 import pathlib
 import subprocess as sub
 import tempfile
-
-import numpy as np
-
-log = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
 class AnalysisResults():
     """Results from an OpenSees analysis.
-    
+
     Parameters
     ----------
     returncode
@@ -27,66 +21,88 @@ class AnalysisResults():
     stdout: str
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(init=False)
 class OpenSeesAnalysis():
     """Wrapper for an OpenSees analysis.
-    
+
     Parameters
     ----------
-    echo_output : optional
-        If True, echo OpenSees output to console. (default: False)
-    delete_files : optional
+    echo_output : bool, optional
+        If True, echo OpenSees output to stdout. (default: False)
+    delete_files : bool, optional
         If True, delete temporary files after each run. (default: True)
-    opensees : Path, optional
-        OpenSees binary to use. (default: 'OpenSees')
-    scratch_dir : optional
-        Directory to store temporary files. (default: system temp directory)
+    opensees_path : pathlib.Path, optional
+        Path to the OpenSees binary to use. If None, looks for 'OpenSees' on the
+        system PATH. (default: None)
+    scratch_path : pathlib.Path, optional
+        Path to the directory for storing temporary files. If None, uses the
+        system temp directory. (default: None)
     """
-    echo_output: bool = False
-    delete_files: bool = True
-    opensees: pathlib.Path = None
-    scratch_dir: pathlib.Path = None
 
-    def __post_init__(self):
-        if self.opensees is None:
-            self.opensees = pathlib.Path('OpenSees')
-        else:
-            self.opensees = pathlib.Path(self.opensees)
+    echo_output: bool
+    delete_files: bool
+    opensees_path: pathlib.Path
+    scratch_path: pathlib.Path
 
-        if self.scratch_dir is None:
-            self.scratch_dir = pathlib.Path(tempfile.gettempdir())
-        else:
-            self.scratch_dir = pathlib.Path(self.scratch_dir)
+    def __init__(self, echo_output=False, delete_files=True, opensees_path=None, scratch_path=None):
+        self.echo_output = echo_output
+        self.delete_files = delete_files
+        self.opensees_path = opensees_path
+        self.scratch_path = scratch_path
 
-        self.working_dir = tempfile.TemporaryDirectory(
-            prefix="OpenSeesAnalysis_", dir=self.scratch_dir)
+    @property
+    def opensees_path(self):
+        """Path to the OpenSees binary to use.
+
+        If None, looks for 'OpenSees' on the system PATH.
+        """
+        return self._opensees_path
+
+    @opensees_path.setter
+    def opensees_path(self, value):
+        if value is None:
+            value = 'OpenSees'
+        self._opensees_path = pathlib.Path(value)
+
+    @property
+    def scratch_path(self):
+        return self._scratch_path
+
+    @scratch_path.setter
+    def scratch_path(self, value):
+        if value is None:
+            value = tempfile.gettempdir()
+        self._scratch_path = pathlib.Path(value)
 
     def scratch_file(self, filename: str) -> pathlib.Path:
         """Return the path to `filename` in the scratch directory."""
-        return pathlib.Path(self.working_dir.name)/filename
+        return self.scratch_path/filename
 
     def run_opensees(self, inputfile: str, echo: bool = None) -> AnalysisResults:
         """Run an OpenSees script.
+
+        Note that all script output is redirected to stdout.
 
         Parameters
         ----------
         inputfile
             Script to execute.
         echo : optional
-            If `True`, echo output to console. If not provided, uses the 
-            `self.echo_output` setting.
+            If True, echo output to console. If not provided, uses the
+            `self.echoOutput` setting.
+
+        Returns
+        -------
+        results : AnalysisResults
+            Completed process information.
         """
         if echo is None:
             echo = self.echo_output
 
-        cmd = [self.opensees, inputfile]
+        cmd = [str(self.opensees_path), inputfile]
         stdout = []
 
-        #pylint: disable=E1123
-        # (bug in astroid.brain; text argument not recognized)
-        log.debug("Calling OpenSees with cmd {}".format(cmd))
         with sub.Popen(cmd, bufsize=1, stdout=sub.PIPE, stderr=sub.STDOUT, text=True) as p:
-            #pylint: enable=E1123
             if echo:
                 for line in p.stdout:
                     print(line, end='')
