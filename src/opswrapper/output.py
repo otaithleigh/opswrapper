43317b4ee0eaf465ce0baa28d3c3ffe9fef00b2d
Peter Talley <peterctalley@gmail.com>
2019-05-13 18:44:50 -0400

    Update recorders with new things from strongback

--- src/opswrapper/output.py
+++ src/opswrapper/output.py
@@ -8,14 +8,22 @@ from . import base
 @dataclasses.dataclass
 class ElementRecorder(base.OpenSeesObject):
     """Element recorder.
-        
+
     All parameters are keyword-only.
 
     Parameters
     ----------
-    file : str
+    file : str, optional
         Filename to record to. This is printed to the file within braces, so no
-        variable substitution on the Tcl side is supported.
+        variable substitution on the Tcl side is supported. If not given,
+        '{{file}}' is printed instead, allowing for substitution later using
+        ``.format(file='/path/to/file')``.
+    fileformat : str, optional
+        File format to use. Options: 'file' (ASCII), 'xml', 'binary'.
+        (default: 'file')
+    precision : int, optional
+        Precision (number of significant figures) of the recorder. If None, uses
+        the default OpenSees precision (6). (default: None)
     elements : array-like
         Element tags to record.
     dofs : array-like, optional
@@ -25,18 +33,27 @@ class ElementRecorder(base.OpenSeesObject):
 
     Example
     -------
-    >>> ElementRecorder(file='/path/to/file', elements=1, dofs=[1, 2], 
+    >>> ElementRecorder(file='/path/to/file', elements=1, dofs=[1, 2],
     ... response='localForce').tcl_code()
     'recorder Element -file {/path/to/file} -ele 1 -dof 1 2 localForce'
     """
-    file: str
-    elements: np.ndarray
-    response: str
+    file: str = None
+    fileformat: str = 'file'
+    precision: int = None
+    elements: np.ndarray = None
     dofs: np.ndarray = None
+    response: str = None
 
     def tcl_code(self) -> str:
-        command = f"recorder Element -file {{{self.file}}}"
+        if self.file is not None:
+            command = f"recorder Element -{self.fileformat} {{{self.file!s}}}"
+        else:
+            command = f"recorder Element -{self.fileformat}" " {{{file!s}}}"
+
         command += " -ele " + " ".join([f"{tag:d}" for tag in np.array(self.elements).flat])
+
+        if self.precision is not None:
+            command += f" -precision {self.precision:d}"
 
         if self.dofs is not None:
             command += " -dof " + " ".join([f"{dof:d}" for dof in np.array(self.dofs).flat])
@@ -49,18 +66,29 @@ class ElementRecorder(base.OpenSeesObject):
 @dataclasses.dataclass
 class NodeRecorder(base.OpenSeesObject):
     """Node recorder.
-    
-    All parameters are keyword-only.
 
     Parameters
     ----------
-    file : str
+    file : str, optional
         Filename to record to. This is printed to the file within braces, so no
-        variable substitution on the Tcl side is supported.
+        variable substitution on the Tcl side is supported. If not given,
+        '{{file}}' is printed instead, allowing for substitution later using
+        ``.format(file='/path/to/file')``. (default: None)
+    fileformat : str, optional
+        File format to use. Options: 'file' (ASCII), 'xml', 'binary'.
+        (default: 'file')
+    precision : int, optional
+        Precision (number of significant figures) of the recorder. If None, uses
+        the default OpenSees precision (6). (default: None)
+    time_series : int, optional
+        Tag of an existing timeSeries. If provided, results from the nodes at each
+        time step are added to the load factor from the series.
     time : bool, optional
         If True, places domain time in the first entry of each line. (default=False)
     nodes : array-like
         Node tags to record.
+    node_range : array-like, optional
+        Pair of node tags that define the range to record.
     dofs : array-like, optional
         DOFs to record.
     response : str
@@ -68,7 +96,7 @@ class NodeRecorder(base.OpenSeesObject):
 
     Valid Responses
     ---------------
-    
+
     ==============  =========================
     Response        Description
     ==============  =========================
@@ -81,28 +109,47 @@ class NodeRecorder(base.OpenSeesObject):
     rayleighForces  damping forces
     ==============  =========================
 
-    Returns
-    -------
-    command : str
-        The formatted OpenSees command to create the element recorder.
-
     Example
     -------
     >>> NodeRecorder(file='/path/to/file', nodes=1, dofs=[1, 2], response='disp').tcl_code()
     'recorder Node -file {/path/to/file} -node 1 -dof 1 2 disp'
     """
-    file: str
-    nodes: np.ndarray
-    response: str
+    file: str = None
+    fileformat: str = 'file'
+    precision: int = None
+    time_series: int = None
     time: bool = False
+    nodes: np.ndarray = None
     dofs: np.ndarray = None
+    response: str = None
+
+    def __post_init__(self):
+        if self.fileformat not in ['file', 'xml', 'binary']:
+            raise ValueError(
+                "NodeRecorder: given file format is not recognized ",
+                f"(expected one of 'file', 'xml', 'binary'; got {self.fileformat!r}"
+            )
 
     def tcl_code(self):
-        command = f"recorder Node -file {{{self.file}}}"
+        if self.file is not None:
+            command = f"recorder Node -{self.fileformat} {{{self.file!s}}}"
+        else:
+            command = f"recorder Node -{self.fileformat}" " {{{file!s}}}"
+
+        if self.precision is not None:
+            command += f" -precision {self.precision:d}"
+
+        if self.time_series is not None:
+            command += f' -timeSeries {self.time_series:d}'
+
         if self.time:
             command += " -time"
 
-        command += " -node " + " ".join([f"{tag:d}" for tag in np.array(self.nodes).flat])
+        if self.nodes is not None:
+            command += " -node " + " ".join([f"{tag:d}" for tag in np.array(self.nodes).flat])
+
+        if self.node_range is not None:
+            command += f" -nodeRange {self.node_range[0]:d} {self.node_range[1]:d}"
 
         if self.dofs is not None:
             command += " -dof " + " ".join([f"{dof:d}" for dof in np.array(self.dofs).flat])
