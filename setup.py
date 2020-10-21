8d52397c5076cc67914f2054c0dbf401cdb82300
Peter Talley <peterctalley@gmail.com>
2020-10-21 06:40:08 +0000

    Move to setup.cfg build system

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="opswrapper",
-    version="0.2.1",
-
-    packages=["opswrapper"],
-    package_dir={'': "src"},
-
-    python_requires=">=3.7",
-    install_requires=[
-        "numpy",
-        "toml",
-        "xarray",
-    ],
-
-    author="Peter Talley",
-    author_email="ptalley2@vols.utk.edu",
-    description="Python helpers for writing OpenSees scripts.",
-    license="MIT",
-)
+setup()
