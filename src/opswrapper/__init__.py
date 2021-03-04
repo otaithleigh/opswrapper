07780e6fbbf1946781f8af5f6c2ddc3da3a703a2
Peter Talley <peterctalley@gmail.com>
2021-03-04 12:00:40 -0500

    Add missing top-level imports

--- src/opswrapper/__init__.py
+++ src/opswrapper/__init__.py
@@ -3,12 +3,15 @@ import importlib.resources
 
 from . import analysis
 from . import base
+from . import config
 from . import element
 from . import formatting
 from . import integration
 from . import material
 from . import output
 from . import section
+from . import uniaxialmaterialanalysis
+from . import utils
 from .base import *
 from .formatting import *
 from .model import *
