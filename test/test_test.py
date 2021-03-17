c4a8861fd3fcf3b144a3b57479af0696936c25f7
Peter Talley <peterctalley@gmail.com>
2021-03-17 07:50:22 -0400

    Add unit tests for 'test'

---
+++ test/test_test.py
@@ -0,0 +1,25 @@
+from opswrapper import test
+
+
+def test_standard():
+    generated = test.NormUnbalance(1e-8, 50).tcl_code()
+    expected = 'test NormUnbalance 1e-08 50 0 2'
+    assert generated == expected
+
+
+def test_with_flags():
+    generated = test.NormUnbalance(1e-8, 50, 2, 2).tcl_code()
+    expected = 'test NormUnbalance 1e-08 50 2 2'
+    assert generated == expected
+
+
+def test_with_str_field():
+    generated = test.NormDispIncr('$tol', 50, 2, 2).tcl_code()
+    expected = 'test NormDispIncr $tol 50 2 2'
+    assert generated == expected
+
+
+def test_alternate_format():
+    generated = test.NormUnbalance(1e-8, 50, 2, 2).tcl_code({float: '.2e'})
+    expected = 'test NormUnbalance 1.00e-08 50 2 2'
+    assert generated == expected
